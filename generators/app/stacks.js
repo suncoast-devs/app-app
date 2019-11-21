2c74dc0b24b9460df8b0236fe2ea9971b2b23355
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-21 09:17:31 -0500

    fixed alignment of stacks

--- generators/app/stacks.js
+++ generators/app/stacks.js
@@ -2,6 +2,6 @@ module.exports = {
   alpha: 'ALPHA: Vanilla stack with HTML, CSS, linting, and BrowserSync',
   beta:
     'BETA:  Vanilla stack with HTML, CSS, linting, JavaScript and BrowserSync',
-  gamma: 'GAMMA:  React stack',
-  delta: 'DELTA: React stack and React Router'
+  gamma: 'GAMMA: React stack',
+  delta: 'DELTA: React stack and React Router',
 }
