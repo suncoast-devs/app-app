0becd14bf1575208040dd8d2ea01161433d10941
Gavin Stark <gavin@gstark.com>
2021-02-01 13:19:41 -0500

    Removes the origin/master as some use a different branch name

--- generators/app/index.js
+++ generators/app/index.js
@@ -209,12 +209,7 @@ class AppApp extends Generator {
       this.spawnCommandSync('git', ['add', '--all'])
       this.spawnCommandSync('git', ['commit', '--message', '"Hello, App App!"'])
       this.spawnCommandSync('hub', ['create', '-h', '', this.appname])
-      this.spawnCommandSync('git', [
-        'push',
-        '--set-upstream',
-        'origin',
-        'master',
-      ])
+      this.spawnCommandSync('git', ['push'])
     }
 
     console.log()
