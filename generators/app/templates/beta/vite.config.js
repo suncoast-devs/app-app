88aaf374eac973f504574a2a60788a5d80fb3941
Gavin Stark <gavin@gstark.com>
2021-06-14 15:30:23 -0400

    Adds ts-check plugin to get typscript errors project wide

--- generators/app/templates/beta/vite.config.js
+++ generators/app/templates/beta/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import TsChecker from 'vite-plugin-ts-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), TsChecker()],
+  plugins: [TsChecker()],
 })
